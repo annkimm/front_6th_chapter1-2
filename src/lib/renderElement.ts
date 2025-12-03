8021a8b846f6ff1bd63a91f9be00fd5b580f994b
annkimm <zizicoo208@naver.com>
2025-12-03 19:36:01 +0900

    feat: add renderElement

--- src/lib/renderElement.ts
+++ src/lib/renderElement.ts
@@ -2,9 +2,22 @@ import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
 import { updateElement } from "./updateElement";
+import { VNodeChild } from "../type";
 
-export function renderElement(vNode, container) {
+export function renderElement(vNode: VNodeChild, container: HTMLElement) {
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
   // 이후에는 updateElement로 기존 DOM을 업데이트한다.
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
+
+  if (!container.firstChild) {
+    const elements = createElement(normalizeVNode(vNode));
+
+    container["_vNode"] = normalizeVNode(vNode);
+    container.append(elements);
+  } else {
+    container = updateElement(container, vNode, container["_vNode"]);
+    container["_vNode"] = normalizeVNode(vNode);
+  }
+
+  return setupEventListeners(container);
 }

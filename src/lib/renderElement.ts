095ad2f22e846891d57885052d818028d7d7cab8
annkimm <zizicoo208@naver.com>
2025-12-06 15:11:08 +0900

    refactor: remove console

--- src/lib/renderElement.ts
+++ src/lib/renderElement.ts
@@ -10,15 +10,14 @@ export function renderElement(vNode: VNodeChild, container: HTMLElement) {
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
 
   if (!container.firstChild) {
-    const elements = createElement(normalizeVNode(vNode));
-    console.log("a");
+    const node = normalizeVNode(vNode);
+    const elements = createElement(node);
 
-    container["_vNode"] = normalizeVNode(vNode);
+    container["_vNode"] = node;
     container.append(elements);
   } else {
     container = updateElement(container, vNode, container["_vNode"]);
     container["_vNode"] = vNode;
-    console.log("b");
   }
 
   return setupEventListeners(container);
